ca4ffa6e066430ec51780b782af0d742dc7b17ba
Abed Islam <versatil@gmail.com>
2018-03-28 03:37:55 -0400

    Lesson 3.10.1 - Hijacking Requests - event.respondWith and Response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', function(event) {
-  console.log('hello world and event: ', event.request);
+  // event.respondWith's argument is either a Response object or a Promise that resolves with a Response.
+  event.respondWith(
+    //new Response('Hello world') // Response's argument can be anything, blob, buffer, simplest thing is string.
+    //new Response('Hello <strong>world</strong>.') // This will print even the HTML tags, b/c the browser will not render it as HTML but as plain text by default.
+    new Response('Hello <strong>world</strong>.', { // The second argument of Response is an object that can set headers.
+      headers: {'foo': 'bar'}
+    })
+  );
 });
