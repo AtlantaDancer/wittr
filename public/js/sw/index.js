0180c74a4c4588f31e909ef902d0e7155d30469b
Abed Islam <versatil@gmail.com>
2018-03-29 04:13:27 -0400

    Lesson 3.11.1: Quiz: Hijacking requests - return actual HTML response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,33 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  
+  // respondWith expects a Promise that resolves to a Response.
+  // So we make a new Promise and resolve it immediately.
+  event.respondWith(
+    new Promise(
+      resolve => resolve(new Response(
+        '<span class="a-winner-is-me">Hello world</span>',
+        {
+          headers: { 'Content-Type': 'text/html; charset=utf-8' }
+        }
+      ))
+    )  
+  );
+  
+  // It is not necessary to return the resolve, this works, too:
+  /*
+  event.respondWith(
+    new Promise(
+      resolve => {
+        resolve(new Response(
+          '<span class="a-winner-is-me">Hello world2</span>',
+          {
+            headers: { 'Content-Type': 'text/html; charset=utf-8' }
+          }
+        ));
+      }
+    )  
+  );
+  */
 });
