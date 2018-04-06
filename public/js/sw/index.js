ac471199e95d1a32b20b1cb8a85aa98284c841f9
Abed Islam <versatil@gmail.com>
2018-04-05 22:36:34 -0400

    Lesson 3.17.1: Quiz: Install and Cache, shiny new SW means cache all the thingz!

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -11,6 +11,20 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: open a cache named 'wittr-static-v1'
     // Add cache the urls from urlsToCache
+    
+    // waitUntil extends the lifetime of an event.
+    // It has 1 param: a Promise!
+    // The method Jake taught to make a new cache, `caches.open`, returns a Promise!
+    caches.open('wittr-static-v1')
+    // However, this Promise resolves to a cache, and we need to do stuff with that.
+    .then(
+      // Now that we've then'd the Promise, its resolution is tied to the fate
+      // of its callback, if a Promise is returned (which is what we want for 
+      // `waitUntil`), the fate of the original Promise is deferred to this new
+      // one. Jake also taught us about `cache.addAll`. It does exactly what we
+      // need to do and it also returns a Promise!
+      wittr_static_cache => wittr_static_cache.addAll(urlsToCache)
+    )
   );
 });
 
