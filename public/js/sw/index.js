289b401e27278b16b430d5a3ba4f438f6422704f
Abed Islam <versatil@gmail.com>
2018-04-06 05:57:33 -0400

    Lesson 3.26.1: Quiz: Caching the page skeleton, only 1 skeleton in this closet!

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/skeleton', // This was originally '/'
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -35,9 +35,23 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond to requests for the root page with
   // the page skeleton from the cache
+  
+  // First off, what does a request for the root page look like?
+  // We will only see it once this updated SW is active and we navigate to the
+  // root page. Refreshing will not trigger a request for the root.
+  //console.log('request: ', event.request.url, new URL(event.request.url));
+  // When the URL's pathname is '/' and its origin is the same as the one we're 
+  // currently on, that's our cue to sub in the page skeleton instead.
+  // 
+  // There's no global window object in the SW. It does have a global location,
+  // which is the exact same as self.location. It also has a self.origin, too.
+  
+  const request_url = new URL(event.request.url); // This allows us to look for '/'.
+  let cache_request = (request_url.pathname === '/' && request_url.origin === self.origin) ? '/skeleton' : event.request;
 
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    // When matching against the cache, if fetching the root, we replaced `event.request` with '/skeleton' above
+    caches.match(cache_request).then(function(response) {
       return response || fetch(event.request);
     })
   );
